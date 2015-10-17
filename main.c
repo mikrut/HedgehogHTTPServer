958841fdd5c507f5be04b03e5a294a0a29b64f46
mikrut <mihanik01@mail.ru>
2015-10-17 13:04:35 +0300

    File support added

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <sys/epoll.h>
 #include <sys/ioctl.h>
@@ -6,21 +7,58 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <netdb.h>
 #include <errno.h>
+#include <libgen.h>
 
 
 #define DBG_INT(var) printf(#var" = %d\n", (var))
 #define DBG_STR(str) puts((str))
 #define DBG_ADR(adr) printf(#adr" = %p\n", (adr))
 
-void write_response(int sockfd,
-                    int status_code, const char* status_string,
-                    const char* content_type,
-                    const char* buf, size_t buflen) {
+const int ext_cmp_len = 5;
+
+int in(const char** array, int arr_len, const char* str) {
+  for (int i = 0; i < arr_len; i++)
+    if (!strncmp(array[i], str, ext_cmp_len))
+      return true;
+  return false;
+}
+
+void ext_to_mime(const char* ext, char* result) {
+  const char* images[] = {"jpg", "jpeg", "png", "gif"};
+  const char* texts[] = {"html", "css"};
+
+  if (ext != NULL) {
+    if (in(images, sizeof(images)/sizeof(images[0]), ext)) {
+      strncpy(result, "image/", 20);
+      strncat(result, ext, ext_cmp_len);
+    } else if (in(texts, sizeof(texts)/sizeof(texts[0]), ext)) {
+      strncpy(result, "text/", 6);
+      strncat(result, ext, ext_cmp_len);
+    } else if (!strncmp(ext, "swf", ext_cmp_len)) {
+      const char swf_mime[] = "application/vnd.adobe.flash-movie";
+      strncpy(result, swf_mime, sizeof(swf_mime));
+    } else if (!strncmp(ext, "js", ext_cmp_len)){
+      const char js_mime[] = "application/javascript";
+      strncpy(result, js_mime, sizeof(js_mime));
+    } else {
+      const char plain_mime[] = "text/plain";
+      strncpy(result, plain_mime, sizeof(plain_mime));
+    }
+  } else {
+    const char plain_mime[] = "text/plain";
+    strncpy(result, plain_mime, sizeof(plain_mime));
+  }
+}
+
+void write_header(int sockfd,
+                 int status_code, const char* status_string,
+                 int content_length, const char* content_type) {
   const char* response_header_template = "\
 HTTP/1.1 %d %s\n\
 Server: HedgehogHTTPServer\n\
@@ -32,16 +70,62 @@ Connection: close\n\
   int header_size = strlen(response_header_template) + 4 + 10 + strlen(content_type);
   char* header = (char*) malloc(sizeof(char) * header_size);
   sprintf(header, response_header_template, status_code, status_string,
-          buflen, content_type);
+          content_length, content_type);
   int header_true_len = strlen(header);
 
-  char* response = (char*) malloc(sizeof(char) * (header_true_len + buflen + 1));
-  memcpy(response, header, header_true_len);
-  memcpy(response + header_true_len, buf, buflen);
-  response[header_true_len + buflen + 1] = '\0';
+  puts(header);
+  write(sockfd, header, header_true_len);
+}
 
-  puts(response);
-  write(sockfd, response, header_true_len + buflen + 1);//, MSG_CONFIRM);
+void write_response(int sockfd,
+                    int status_code, const char* status_string,
+                    const char* content_type,
+                    const char* buf, size_t buflen) {
+  write_header(sockfd, status_code, status_string, buflen, content_type);
+
+  puts(buf);
+  write(sockfd, buf, buflen);
+}
+
+void write_file_response(char* file_path, int sockfd) {
+  char* name = basename(file_path);
+  const char* ext = strrchr(name, '.');
+  char mime_buf[64];
+  if ((ext != NULL)) {
+    ext_to_mime(ext+1, mime_buf);
+  }
+
+  int fd = open(file_path, O_RDONLY);
+  if (fd != -1) {
+    struct stat file_stat;
+    fstat(fd, &file_stat);
+    int file_len = file_stat.st_size;
+
+    write_header(sockfd, 200, "OK", file_len, mime_buf);
+
+    const int READ_BUF_SIZE = 1024;
+    char read_buf[READ_BUF_SIZE];
+    int read_size, left_size;
+
+    do {
+      left_size = read_size = read(fd, read_buf, READ_BUF_SIZE);
+      char* p = read_buf;
+
+      while(left_size) {
+        int written = write(sockfd, read_buf, read_size);
+        left_size -= written;
+        p += written;
+      }
+    } while(read_size > 0);
+
+  } else {
+    const char not_found_message[] =
+      "Requested file was not found on this server";
+    write_response(sockfd,
+      404, "Not Found",
+      "text/plain",
+      not_found_message, sizeof(not_found_message));
+  }
 }
 
 int setnonblocking(int fd) {
@@ -122,11 +206,14 @@ int main(void) {
         DBG_ADR(&events[n]);
 
         int input_len = read(events[n].data.fd, buf, 245);
-        puts(buf);
+        if (input_len > 0) {
+          puts(buf);
 
-        const char* resp = "Hello, world!";
-        write_response(events[n].data.fd, 200, "OK", "text/html", resp, strlen(resp));
-        close(events[n].data.fd);
+          write_file_response("index.html", events[n].data.fd);
+          //const char* resp = "Hello, world!";
+          //write_response(events[n].data.fd, 200, "OK", "text/html", resp, strlen(resp));
+          close(events[n].data.fd);
+        }
       }
     }
   }
